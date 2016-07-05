2b61961f985f0958ba9352d1e9f38489ebc43742
Robert Strandh <robert.strandh@gmail.com>
2016-07-05 15:42:18 +0200

    New component containg the code for this library.

---
+++ ducling.lisp
@@ -0,0 +1,21 @@
+(cl:in-package #:ducling)
+
+(defclass map () ())
+  
+
+(defclass hash-table-map (map)
+  ((%contents :initarg :contents :reader contents)))
+
+(defclass alist-map (map)
+  ((%test :initarg :test :reader test)
+   (%contents :initarg :contents :accessor contents)))
+
+(defmethod initialize-instance :after ((map map) &key (test #'eql))
+  (if (member test (list #'eq #'eql #'equal #'equalp
+			 'eq 'eql 'equal 'equalp))
+      (change-class map 'hash-table-map
+		    :contents (make-hash-table :test test))
+      (change-class map 'alist-map
+		    :contents '()
+		    :test test)))
+
