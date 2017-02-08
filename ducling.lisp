f158a3841b86c8b85fd059b0b1f07f290994fb5a
Robert Strandh <robert.strandh@gmail.com>
2017-02-08 18:05:13 +0100

    Untabify source for better compatibility with tools.

--- ducling.lisp
+++ ducling.lisp
@@ -12,12 +12,12 @@
 
 (defmethod initialize-instance :after ((map map) &key (test #'eql))
   (if (member test (list #'eq #'eql #'equal #'equalp
-			 'eq 'eql 'equal 'equalp))
+                         'eq 'eql 'equal 'equalp))
       (change-class map 'hash-table-map
-		    :contents (make-hash-table :test test))
+                    :contents (make-hash-table :test test))
       (change-class map 'alist-map
-		    :contents '()
-		    :test test)))
+                    :contents '()
+                    :test test)))
 
 ;;; A map does not directly contain leaf objects or other maps.  If it
 ;;; did, it would not be possible to have a map as a leaf object.
@@ -36,15 +36,15 @@
 (defmethod add-map ((map-to-add map) (into-map alist-map) key)
   (with-accessors ((contents contents) (test test)) into-map
     (let ((existing-cell (assoc key contents :test test))
-	  (entry (make-instance 'map-entry :value map-to-add)))
+          (entry (make-instance 'map-entry :value map-to-add)))
       (if (null existing-cell)
-	  (push (cons key entry) contents)
-	  (setf (cdr existing-cell) entry))))
+          (push (cons key entry) contents)
+          (setf (cdr existing-cell) entry))))
   map-to-add)
 
 (defmethod add-map ((map-to-add map) (into-map hash-table-map) key)
   (setf (gethash key (contents into-map))
-	(make-instance 'map-entry :value map-to-add))
+        (make-instance 'map-entry :value map-to-add))
   map-to-add)
 
 (defgeneric add-object (object into-map key))
@@ -52,15 +52,15 @@
 (defmethod add-object (object (into-map alist-map) key)
   (with-accessors ((contents contents) (test test)) into-map
     (let ((existing-cell (assoc key contents :test test))
-	  (entry (make-instance 'object-entry :value object)))
+          (entry (make-instance 'object-entry :value object)))
       (if (null existing-cell)
-	  (push (cons key entry) contents)
-	  (setf (cdr existing-cell) entry))))
+          (push (cons key entry) contents)
+          (setf (cdr existing-cell) entry))))
   object)
 
 (defmethod add-object (object (into-map hash-table-map) key)
   (setf (gethash key (contents into-map))
-	(make-instance 'object-entry :value object))
+        (make-instance 'object-entry :value object))
   object)
 
 (defclass state ()
@@ -93,13 +93,13 @@
 
 (defmethod advance-in-map ((map alist-map) key path)
   (let* ((cell (assoc key (contents map) :test (test map)))
-	 (entry (cdr cell))
-	 (new-path (append path (list key))))
+         (entry (cdr cell))
+         (new-path (append path (list key))))
     (advance-with-entry entry new-path)))
 
 (defmethod advance-in-map ((map hash-table-map) key path)
   (let ((entry (gethash key (contents map)))
-	(new-path (append path (list key))))
+        (new-path (append path (list key))))
     (advance-with-entry entry new-path)))
 
 (defgeneric advance-with-entry (entry path))
@@ -126,7 +126,7 @@
 
 (defmethod possible-completions-in-map ((map hash-table-map))
   (loop for key being each hash-key of (contents map)
-	collect key))
+        collect key))
 
 (defmethod possible-completions-in-map ((map alist-map))
   (mapcar #'car (contents map)))
